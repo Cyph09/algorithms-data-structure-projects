9d7a2bd719b8a564ee6ebabc74556ab209292e9d
Swaleh Mhina <cyph09@gmail.com>
2019-07-17 23:01:10 +0300

    Ceasers Cipher(ROT13)

--- main.js
+++ main.js
@@ -25,7 +25,40 @@ function palindrome(str) {
     .reduce((joinedStr, el) => joinedStr + el);
   return reverseString === initialString;
 }
-document.write(palindrome("e%Ye"));
-console.log(palindrome("raCE CA-R  "));
+// document.write(palindrome("e%Ye"));
+// console.log(palindrome("raCE CA-R  "));
 
 //   #############-END PALINDROME CHEKER-############
+
+// #############-CAESARS CIPHER-############
+/**
+ * A common modern use is the ROT13 cipher, where the values of the
+ * letters are shifted by 13 places. Thus 'A' ↔ 'N', 'B' ↔ 'O' and so on.
+ *
+ * Write a function which takes a ROT13 encoded string as input and
+ * returns a decoded string.
+ *
+ * All letters will be uppercase. Do not transform any non-alphabetic
+ * character (i.e. spaces, punctuation), but do pass them on.
+ */
+function rot13(str) {
+  // LBH QVQ VG!
+  let charCodes = [];
+  for (i = 0; i < str.length; i++) {
+    let charCode = str.toUpperCase().charCodeAt(i);
+    if (charCode >= 65 && charCode <= 77) {
+      charCode = charCode + 13;
+      charCodes.push(charCode);
+    } else if (charCode >= 78 && charCode <= 90) {
+      charCode = charCode - 13;
+      charCodes.push(charCode);
+    } else {
+      charCodes.push(charCode);
+    }
+  }
+  let newWord = String.fromCharCode(...charCodes);
+  // console.log(charCodes);
+  // console.log(newWord);
+  return newWord;
+}
+rot13("v'ir 100% qbar vg");
